94c3a13795508173cbc31a186235933e59a88e14
Lailah Amazraawi <lailahamazraawi@Lailahs-MacBook-Air.local>
2022-03-20 18:39:08 -0400

    completedcalculator

---
+++ test.js
@@ -0,0 +1,9 @@
+function addNumbers() {
+    var firstNumber = document.getElementById('firstNumber').value
+    var secondNumber = document.getElementById('secondNumber').value
+    var answer = Number(firstNumber) + Number(secondNumber)
+
+    document.getElementById('answerBox').innerHTML = answer
+}
+
+

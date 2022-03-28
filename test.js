7743a81435e633eadbf21cc2eac2f60c4bdc4ecb
Lailah Amazraawi <lailahamazraawi@Lailahs-MacBook-Air.local>
2022-03-27 21:20:43 -0400

    web cal 2 complete

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
-function addNumbers() {
+function calculateNumbers() {
     var firstNumber = document.getElementById('firstNumber').value
     var secondNumber = document.getElementById('secondNumber').value
-    var answer = Number(firstNumber) + Number(secondNumber)
+    var operation = document.getElementById('operation').value
+    var answer = 0
+
+    if(operation === "plus"){
+        answer = Number(firstNumber) + Number(secondNumber)
+    }
+    if (operation === "minus"){
+        answer = Number(firstNumber) - Number(secondNumber)
+    }
+    if (operation === "divide"){
+        answer = Number(firstNumber) / Number(secondNumber)
+    }
+    if (operation === "multiply"){
+        answer = Number(firstNumber) * Number(secondNumber)
+    }
+
+    
 
     document.getElementById('answerBox').innerHTML = answer
 }
-
-
